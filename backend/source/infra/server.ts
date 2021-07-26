95b2d9b2c3a635b04ef9aed7a24187a270e85a6c
Abraão Duarte <abraao.n.duarte@gmail.com>
2021-07-26 09:28:49 -0300

    feat: Add cors to project

--- backend/source/infra/server.ts
+++ backend/source/infra/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { api } from '../app/api';
 import { handleError, handleNotFound } from '../app/middlewares';
 
 const server = express();
 
+server.use(cors());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use(api);
